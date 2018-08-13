41c9e1e1dc13a0e7697341e5af491ac92e689dd2
willcat <yunxiuhe@catchingmedia.com>
2018-08-13 11:44:40 +0800

    new ignore

--- src/main/java/org/willcat/flink/inspectFinance/App.java
+++ src/main/java/org/willcat/flink/inspectFinance/App.java
@@ -1,5 +1,7 @@
 package org.willcat.flink.inspectFinance;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     }
 }
