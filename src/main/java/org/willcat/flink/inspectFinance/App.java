0306e86b47e3b627dbe6f5a5679e12b9f01cc419
willcat <yunxiuhe@catchingmedia.com>
2018-08-13 11:16:02 +0800

    init

---
+++ src/main/java/org/willcat/flink/inspectFinance/App.java
@@ -0,0 +1,13 @@
+package org.willcat.flink.inspectFinance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
